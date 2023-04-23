0530f9c30af5a4f408cbafeda1fb497caef765e8
Cristiano Sampaio <53228396+CristianoS16@users.noreply.github.com>
2023-04-23 19:46:45 -0300

    Feat/add general serializer (#5)
    * feat(admin serializer): add first version
    
    * refactor: improve some details on admin serializer
    
    * feat(lib): add cJSON library
    
    * feat(general serializer): add serializer to manager user and admin serializer

--- utils/operation_codes.h
+++ utils/operation_codes.h
@@ -1,26 +1,26 @@
 // Enumeration of possible actions that can be performed by the user.
 typedef enum {
-    // Used to list all people (email and name) who have graduated in a certain course.
+    // 1- Used to list all people (email and name) who have graduated in a certain course.
     LIST_BY_COURSE = 1, 
-    // Used to list all people (email and name) who have a certain skill.
+    // 2- Used to list all people (email and name) who have a certain skill.
     LIST_BY_SKILL, 
-    // Used to list all people (email, name, and course) who have graduated in a certain year.
+    // 3- Used to list all people (email, name, and course) who have graduated in a certain year.
     LIST_BY_YEAR, 
-    // Used to list all the information of all profiles.
+    // 4- Used to list all the information of all profiles.
     LIST_ALL_PROFILES, 
-    // Used to retrieve a profile's information based on its email.
+    // 5- Used to retrieve a profile's information based on its email.
     GET_PROFILE_BY_EMAIL, 
-    // Used to download a profile's image based on its identifier (UDP only).
+    // 6- Used to download a profile's image based on its identifier (UDP only).
     DOWNLOAD_PROFILE_IMAGE, 
     
     // ADMIN ACTIONS
-    // Used to remove a profile based on its email (admin only).
+    // 7- Used to remove a profile based on its email (admin only).
     REMOVE_PROFILE_BY_EMAIL, 
-    // Used to create a new profile using an email as an identifier (admin only).
+    // 8- Used to create a new profile using an email as an identifier (admin only).
     NEW_PROFILE, 
-    // Used to indicate that the user wishes to log in (admin only).
+    // 9- Used to indicate that the user wishes to log in (admin only).
     LOGIN, 
-    // Used to indicate that the user wishes to exit the program (admin only).
+    // 10- Used to indicate that the user wishes to exit the program (admin only).
     LOGOUT 
 } OperationCode;
 
