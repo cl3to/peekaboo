15d99b7542582dad3308a17af2cd60150ed344d0
Cristiano <cristianosampaio16@gmail.com>
2023-06-04 00:25:08 -0300

    improve serializer .h comments

--- server/serializers/user_serializer.h
+++ server/serializers/user_serializer.h
@@ -15,33 +15,33 @@
 extern int should_use_tcp;
 
 // Serialization of the profiles filtred by the academic degree
-// Returns the profiles in json format
-// Returns -1 if error occurred
+// Returns response_stream struct with profiles in json format
+// Returns response_stream struct with a error message if error occurred
 response_stream *profiles_by_course_serializer(Profile *profiles, char *course);
 
 // Serialization of the profiles filtred by the skills
-// Returns the profiles in json format
-// Returns -1 if error occurred
+// Returns response_stream struct with profiles in json format
+// Returns response_stream struct with a error message if error occurred
 response_stream *profiles_by_skill_serializer(Profile *profiles, char *skills);
 
 // Serialization of the profiles filtred by the year of degree
-// Returns the profiles in json format
-// Returns -1 if error occurred
+// Returns response_stream struct with profiles in json format
+// Returns response_stream struct with a error message if error occurred
 response_stream *profiles_by_year_of_degree_serializer(Profile *profiles, int year_of_degree);
 
 // Serialization of all profiles in the database
-// Returns the profiles in json format
-// Returns -1 if error occurred
+// Returns response_stream struct with profiles in json format
+// Returns response_stream struct with a error message if error occurred
 response_stream *profiles_serializer(Profile *profiles);
 
 // Serialization of one profile filteres by e-mail
-// Returns the profiles in json format
-// Returns -1 if error occurred
+// Returns response_stream struct with profiles in json format
+// Returns response_stream struct with a error message if error occurred
 response_stream *profile_by_email_serializer(Profile *profile, char *email);
 
 // Serialization of a profile image
-// Returns the image and a header of 8 bytes
-// Returns a error response if error occurred
+// Returns response_stream struct with image and a header of 8 bytes
+// Returns response_stream struct with error response if error occurred
 response_stream *image_by_email(char *email);
 
 #endif
